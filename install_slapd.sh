bd820e28b8630620e2e74d1de872bacf4ab35219
salim8112 <53063576+salim8112@users.noreply.github.com>
2019-07-25 21:32:27 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -1,10 +1,11 @@
 export DEBIAN_FRONTEND='non-interactive'
 
-echo -e "slapd slapd/password1 password" |debconf-set-selections
-echo -e "slapd slapd/internal/adminpw  password" |debconf-set-selections
+echo -e "slapd slapd/root_password password admin" |debconf-set-selections
+echo -e "slapd slapd/password1 password admin" |debconf-set-selections
+echo -e "slapd slapd/internal/adminpw  password admin" |debconf-set-selections
 
-echo -e "slapd slapd/password2" |debconf-set-selections
-echo -e "slapd slapd/internal/generated_adminpw  password" |debconf-set-selections  
+echo -e "slapd slapd/password2 password admin" |debconf-set-selections
+echo -e "slapd slapd/internal/generated_adminpw  password admin" |debconf-set-selections  
 echo -e "slapd slapd/backend   select  MDB" |debconf-set-selections
 echo -e "slapd slapd/move_old_database boolean true" |debconf-set-selections
 echo -e "slapd slapd/invalid_config    boolean true" |debconf-set-selections
