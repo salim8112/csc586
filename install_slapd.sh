686734c7efcf426e51fe81a8929c1e8733bae26b
salim8112 <53063576+salim8112@users.noreply.github.com>
2019-07-25 21:06:35 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -1,1 +1,27 @@
-testing 
+export DEBIAN_FRONTEND='non-interactive'
+
+echo -e "slapd slapd/password1 password" |debconf-set-selections
+echo -e "slapd slapd/internal/adminpw  password" |debconf-set-selections
+
+echo -e "slapd slapd/password2" |debconf-set-selections
+echo -e "slapd slapd/internal/generated_adminpw  password" |debconf-set-selections  
+echo -e "slapd slapd/backend   select  MDB" |debconf-set-selections
+echo -e "slapd slapd/move_old_database boolean true" |debconf-set-selections
+echo -e "slapd slapd/invalid_config    boolean true" |debconf-set-selections
+echo -e "slapd slapd/dump_database   select  when needed" |debconf-set-selections
+echo -e "slapd slapd/domain    string  clemson.cloudlab.us" |debconf-set-selections
+echo -e "slapd slapd/dump_database_destdir string  /var/backups/slapd-VERSION" |debconf-set-selections
+echo -e "slapd slapd/no_configuration  boolean false" |debconf-set-selections
+echo -e "slapd slapd/ppolicy_schema_needs_update   select  abort installation" |debconf-set-selections
+echo -e "slapd slapd/unsafe_selfwrite_acl      note" |debconf-set-selections
+echo -e "slapd   shared/organization     string  clemson.cloudlab.us" |debconf-set-selections
+echo -e "slapd slapd/purge_database    boolean false" |debconf-set-selections
+
+# Grab slapd and ldap-utils (pre-seeded)
+sudo apt-get update
+apt-get install -y slapd ldap-utils
+
+# Must reconfigure slapd for it to work properly 
+sudo dpkg-reconfigure slapd 
+sudo ufw allow ldap
+
