cb7fa7f424300d9612371278eb601d800429a1cf
salim8112 <53063576+salim8112@users.noreply.github.com>
2019-07-26 15:29:50 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -1,22 +1,24 @@
 export DEBIAN_FRONTEND='non-interactive'
 
 echo -e "slapd slapd/root_password password test" |debconf-set-selections
-echo -e "slapd slapd/password1 password test" |debconf-set-selections
 echo -e "slapd slapd/internal/adminpw  password test" |debconf-set-selections
-
 echo -e "slapd slapd/password2 password test" |debconf-set-selections
-echo -e "slapd slapd/internal/generated_adminpw  password test" |debconf-set-selections  
-echo -e "slapd slapd/backend   select  MDB" |debconf-set-selections
-echo -e "slapd slapd/move_old_database boolean true" |debconf-set-selections
-echo -e "slapd slapd/invalid_config    boolean true" |debconf-set-selections
-echo -e "slapd slapd/dump_database   select  when needed" |debconf-set-selections
-echo -e "slapd slapd/domain    string  clemson.cloudlab.us" |debconf-set-selections
-echo -e "slapd slapd/dump_database_destdir string  /var/backups/slapd-VERSION" |debconf-set-selections
-echo -e "slapd slapd/no_configuration  boolean false" |debconf-set-selections
+echo -e "slapd slapd/internal/generated_adminpw  password test" |debconf-set-selections
+echo -e "slapd slapd/password1 password test" |debconf-set-selections 
+echo -e "slapd slapd/domain  string  clemson.cloudlab.us" |debconf-set-selections
+echo -e "slapd shared/organization  string  clemson.cloudlab.us" |debconf-set-selections
+echo -e "slapd slapd/unsafe_selfwrite_acl  note" |debconf-set-selections
 echo -e "slapd slapd/ppolicy_schema_needs_update   select  abort installation" |debconf-set-selections
-echo -e "slapd slapd/unsafe_selfwrite_acl      note" |debconf-set-selections
-echo -e "slapd   shared/organization     string  clemson.cloudlab.us" |debconf-set-selections
-echo -e "slapd slapd/purge_database    boolean false" |debconf-set-selections
+echo -e "slapd slapd/password_mismatch note" |debconf-set-selections
+echo -e "slapd slapd/backend  select  MDB" |debconf-set-selections
+echo -e "slapd slapd/purge_database boolean false" |debconf-set-selections
+echo -e "slapd slapd/dump_database_destdir string  /var/backups/slapd-VERSION" |debconf-set-selections
+echo -e "slapd slapd/move_old_database boolean true" |debconf-set-selections
+echo -e "slapd slapd/upgrade_slapcat_failure error" |debconf-set-selections
+echo -e "slapd slapd/dump_database   select  when needed" |debconf-set-selections
+echo -e "slapd slapd/allow_ldap_v2 boolean false" |debconf-set-selections
+echo -e "slapd slapd/no_configuration  boolean false" |debconf-set-selections
+echo -e "slapd slapd/invalid_config    boolean true" |debconf-set-selections
 
 # Grab slapd and ldap-utils (pre-seeded)
 sudo apt-get update
@@ -25,4 +27,3 @@ sudo apt-get install -y slapd ldap-utils
 # Must reconfigure slapd for it to work properly 
 sudo dpkg-reconfigure slapd 
 sudo ufw allow ldap
-
