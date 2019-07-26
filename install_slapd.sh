f8690c42ac9c4a98a77d95918c095598a90758ae
salim8112 <53063576+salim8112@users.noreply.github.com>
2019-07-26 15:47:32 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -1,9 +1,9 @@
 export DEBIAN_FRONTEND='non-interactive'
 
 echo -e "slapd slapd/root_password password test" |debconf-set-selections
-echo -e "slapd slapd/internal/adminpw  password test" |debconf-set-selections
-echo -e "slapd slapd/password2 password test" |debconf-set-selections
 echo -e "slapd slapd/internal/generated_adminpw  password test" |debconf-set-selections
+echo -e "slapd slapd/password2 password test" |debconf-set-selections
+echo -e "slapd slapd/internal/adminpw  password test" |debconf-set-selections
 echo -e "slapd slapd/password1 password test" |debconf-set-selections 
 echo -e "slapd slapd/domain  string  clemson.cloudlab.us" |debconf-set-selections
 echo -e "slapd shared/organization  string  clemson.cloudlab.us" |debconf-set-selections
