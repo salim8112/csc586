46bc99c5e8c87be9d8a9cb0d3eab1b18b2731ea5
salim8112 <53063576+salim8112@users.noreply.github.com>
2019-07-26 20:26:46 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -4,19 +4,19 @@ sudo apt-get update
 export DEBIAN_FRONTEND=noninteractive
 
 echo -e " 
-slapd slapd/internal/generated_adminpw password abcd123
+slapd slapd/internal/generated_adminpw password test
 slapd slapd/no_configuration boolean false
 slapd slapd/invalid_config boolean true
 slapd slapd/domain string wisc.cloudlab.us
 slapd slapd/organization string clemson.cloudlab.us
-slapd slapd/internal/adminpw password abcd123
+slapd slapd/internal/adminpw password test
 slapd slapd/backend select MDB
 slapd slapd/purge_database boolean true
 slapd slapd/dump_databse_destdir string /var/backups/slapd-VERSION
 slapd slapd/dump_databse select when needed
 slapd slapd/move_old_database boolean true
-slapd slapd/password2 password abcd123
-slapd slapd/password1 password abcd123
+slapd slapd/password2 password test
+slapd slapd/password1 password test
 slapd slapd/password_mismatch note
 slapd slapd/policy_schema_needs_update select abort installation
 slapd slapd/unsafe_selfwrite_acl note
