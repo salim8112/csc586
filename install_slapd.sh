566989449781574befa8d5db46e210b71b82aeb2
salim8112 <53063576+salim8112@users.noreply.github.com>
2019-07-29 11:35:30 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -3,26 +3,25 @@
 sudo apt-get update
 export DEBIAN_FRONTEND=noninteractive
 
-#echo -e "slapd slapd/root_password password test" |sudo debconf-set-selections
-#echo -e "slapd slapd/root_password_again password test" |sudo debconf-set-selections
+echo -e "slapd slapd/root_password password test" |sudo debconf-set-selections
+echo -e "slapd slapd/root_password_again password test" |sudo debconf-set-selections
 echo -e "slapd slapd/internal/generated_adminpw  password test" |sudo debconf-set-selections
-echo -e "slapd slapd/password2 password test" |sudo debconf-set-selections
+echo -e "slapd slapd/password1 password test" |sudo debconf-set-selections
 echo -e "slapd slapd/internal/adminpw  password test" |sudo debconf-set-selections
-echo -e "slapd slapd/password1 password test" |sudo debconf-set-selections 
-echo -e "slapd slapd/domain  string  clemson.cloudlab.us" |sudo debconf-set-selections
-echo -e "slapd shared/organization  string  clemson.cloudlab.us" |sudo debconf-set-selections
+echo -e "slapd slapd/password2 password test" |sudo debconf-set-selections
 echo -e "slapd slapd/unsafe_selfwrite_acl  note" |sudo debconf-set-selections
-echo -e "slapd slapd/policy_schema_needs_update   select  abort installation" |sudo debconf-set-selections
-echo -e "slapd slapd/password_mismatch note" |sudo debconf-set-selections
-echo -e "slapd slapd/backend  select  MDB" |sudo debconf-set-selections
-echo -e "slapd slapd/purge_database boolean true" |sudo debconf-set-selections
-echo -e "slapd slapd/dump_database_destdir string  /var/backups/slapd-VERSION" |sudo debconf-set-selections
 echo -e "slapd slapd/move_old_database boolean true" |sudo debconf-set-selections
+echo -e "slapd slapd/backend  select  MDB" |sudo debconf-set-selections
+echo -e "slapd slapd/dump_database_destdir string  /var/backups/slapd-VERSION" |sudo debconf-set-selections
+echo -e "slapd slapd/purge_database boolean true" |sudo debconf-set-selections
+echo -e "slapd slapd/password_mismatch note" |sudo debconf-set-selections 
+echo -e "slapd slapd/domain  string  clemson.cloudlab.us" |sudo debconf-set-selections
 echo -e "slapd slapd/upgrade_slapcat_failure error" |sudo debconf-set-selections
 echo -e "slapd slapd/dump_database   select  when needed" |sudo debconf-set-selections
-echo -e "slapd slapd/allow_ldap_v2 boolean false" |sudo debconf-set-selections
 echo -e "slapd slapd/no_configuration  boolean false" |sudo debconf-set-selections
+echo -e "slapd slapd/ppolicy_schema_needs_update   select  abort installation" |sudo debconf-set-selections
 echo -e "slapd slapd/invalid_config    boolean true" |sudo debconf-set-selections
+echo -e "slapd shared/organization  string  clemson.cloudlab.us" |sudo debconf-set-selections
 
 # Grab slapd and ldap-utils (pre-seeded)
 sudo apt-get install -y slapd ldap-utils
