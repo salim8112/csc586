3877e6edcd3249b6888f87ac5099646a4e7413fd
salim8112 <53063576+salim8112@users.noreply.github.com>
2019-07-28 21:54:00 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -2,10 +2,10 @@
 
 sudo apt-get update
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install -y debconf-utils
+#sudo apt-get install -y debconf-utils
 
-echo -e "slapd slapd/root_password password test" |sudo debconf-set-selections
-echo -e "slapd slapd/root_password_again password test" |sudo debconf-set-selections
+#echo -e "slapd slapd/root_password password test" |sudo debconf-set-selections
+#echo -e "slapd slapd/root_password_again password test" |sudo debconf-set-selections
 echo -e "slapd slapd/internal/generated_adminpw  password test" |sudo debconf-set-selections
 echo -e "slapd slapd/password2 password test" |sudo debconf-set-selections
 echo -e "slapd slapd/internal/adminpw  password test" |sudo debconf-set-selections
