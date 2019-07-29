7f2e7223ceab42ee0ca04621bb3d84e61d104237
salim8112 <53063576+salim8112@users.noreply.github.com>
2019-07-29 12:31:07 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
 sudo apt-get update
+sudo apt-get install -y slapd ldap-utils
 export DEBIAN_FRONTEND=noninteractive
 
-
-#echo -e "slapd slapd/root_password password test" |sudo debconf-set-selections
-#echo -e "slapd slapd/root_password_again password test" |sudo debconf-set-selections
+echo -e "slapd slapd/root_password password test" |sudo debconf-set-selections
+echo -e "slapd slapd/root_password_again password test" |sudo debconf-set-selections
+sudo dpkg-reconfigure slapd 
 echo -e "slapd slapd/internal/generated_adminpw  password test" |sudo debconf-set-selections
-echo -e "slapd slapd/password1 password test" |sudo debconf-set-selections
-echo -e "slapd slapd/internal/adminpw  password test" |sudo debconf-set-selections
-echo -e "slapd slapd/password2 password test" |sudo debconf-set-selections
-echo -e "slapd slapd/unsafe_selfwrite_acl  note" |sudo debconf-set-selections
-echo -e "slapd slapd/move_old_database boolean true" |sudo debconf-set-selections
-echo -e "slapd slapd/backend  select  MDB" |sudo debconf-set-selections
-echo -e "slapd slapd/dump_database_destdir string  /var/backups/slapd-VERSION" |sudo debconf-set-selections
-echo -e "slapd slapd/purge_database boolean true" |sudo debconf-set-selections
-echo -e "slapd slapd/password_mismatch note" |sudo debconf-set-selections 
-echo -e "slapd slapd/domain  string  clemson.cloudlab.us" |sudo debconf-set-selections
-echo -e "slapd slapd/upgrade_slapcat_failure error" |sudo debconf-set-selections
-echo -e "slapd slapd/dump_database   select  when needed" |sudo debconf-set-selections
 echo -e "slapd slapd/no_configuration  boolean false" |sudo debconf-set-selections
-echo -e "slapd slapd/ppolicy_schema_needs_update   select  abort installation" |sudo debconf-set-selections
 echo -e "slapd slapd/invalid_config    boolean true" |sudo debconf-set-selections
+echo -e "slapd slapd/domain  string  clemson.cloudlab.us" |sudo debconf-set-selections
 echo -e "slapd shared/organization  string  clemson.cloudlab.us" |sudo debconf-set-selections
+echo -e "slapd slapd/internal/adminpw  password test" |sudo debconf-set-selections
+echo -e "slapd slapd/backend  select  MDB" |sudo debconf-set-selections
+echo -e "slapd slapd/purge_database boolean true" |sudo debconf-set-selections
+echo -e "slapd slapd/dump_database_destdir string  /var/backups/slapd-VERSION" |sudo debconf-set-selections
+echo -e "slapd slapd/dump_database   select  when needed" |sudo debconf-set-selections
+echo -e "slapd slapd/move_old_database boolean true" |sudo debconf-set-selections
+echo -e "slapd slapd/password2 password test" |sudo debconf-set-selections
+echo -e "slapd slapd/password1 password test" |sudo debconf-set-selections
+echo -e "slapd slapd/password_mismatch note" |sudo debconf-set-selections
+echo -e "slapd slapd/ppolicy_schema_needs_update   select  abort installation" |sudo debconf-set-selections
+echo -e "slapd slapd/unsafe_selfwrite_acl  note" |sudo debconf-set-selections
+echo -e "slapd slapd/upgrade_slapcat_failure error" |sudo debconf-set-selections
+echo -e "slapd slapd/allow_ldap_v2 boolean false" |sudo debconf-set-selections
 
 # Grab slapd and ldap-utils (pre-seeded)
-sudo apt-get install -y slapd ldap-utils
+#sudo apt-get install -y slapd ldap-utils
 # Must reconfigure slapd for it to work properly 
-sudo dpkg-reconfigure slapd 
+#sudo dpkg-reconfigure slapd 
 sudo ufw allow ldap
-ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w test -f /local/repository/basedn.ldif
+#ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w test -f /local/repository/basedn.ldif
 
 PASS=$(slappasswd -s rammy)
 cat <<EOF >/local/repository/users.ldif
@@ -50,4 +52,4 @@ loginShell: /bin/dash
 homeDirectory: /home/student
 EOF
 
-ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w test -f /local/repository/users.ldif
+#ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w test -f /local/repository/users.ldif
